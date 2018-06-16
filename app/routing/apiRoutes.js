060416d7fd966add87a0331a582a90467b9090f7
Stanley Louis <stanlouis@msn.com>
2018-06-16 10:29:56 -0400

    Send translation back to index.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,46 @@
 var textToSpeech = require("../utils");
+const translate = require("google-translate-api");
 
-module.exports = function (app) {
-    app
-        .get("/api/phrase", function (req, res) {
-            res.json({"name": "Stanley"});
-        });
+module.exports = function(app) {
+  app.get("/api/phrase", function(req, res) {
+    res.json({ name: "Stanley" });
+  });
 
-    app.post("/api/phrase", function (req, res) {
-        var data = req.body;
-        var language = data.language;
-        if (language === "French") {
-            language = "fr-FR"
-        } else if (language === "Japanese") {
-            language = "ja-JP"
-        } else if (language === "Spanish") {
-            language === "es-ES"
-        }
-        var phrase = data.phrase;
+  app.post("/api/phrase", function(req, res) {
+    var data = req.body;
+    var language = data.language;
+    if (language === "English") {
+      language = "en-US";
+    } else if (language === "French") {
+      language = "fr-FR";
+    } else if (language === "Japanese") {
+      language = "ja-JP";
+    } else if (language === "Spanish") {
+      language = "es-ES";
+    }
+    var phrase = data.phrase;
 
-        textToSpeech(phrase, language);
+    var resultPhrase = translate(phrase, { to: language.slice(0, 2) })
+      .then(res => {
+        var translatedPhrase = res.text;
+        // console.log(res.text);
 
-        // res.sendFile('../output.mp3');
-        res.send('Hello');
+        //=> text to speech
+        // textToSpeech(translatedPhrase, language);
+        return translatedPhrase;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
+    // textToSpeech(phrase, language);
+
+    // res.sendFile('../output.mp3');
+  resultPhrase.then(function(result) {
+     data.phrase = result;
+     res.json(data); // "Stuff worked!"
+    }, function(err) {
+      console.log(err); // Error: "It broke"
     });
+  });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,13 @@
 var path = require("path");
-var express = require('express');
+var express = require("express");
 
 // var publicPath = path.resolve(__dirname, "/app/public");
-module.exports = function (app) {
-    // app     .us("*", function (req, res) { res.sendFile(path.join(__dirname,
-    // "/../public/text.html"));     });
-    app
-        .get("/", function (req, res) {
-            res.sendFile(path.join(__dirname + "/../public/index.html"))
-            console.log(__dirname)
-        })
-    // app.use(express.static(publicPath))
+module.exports = function(app) {
+  // app     .us("*", function (req, res) { res.sendFile(path.join(__dirname,
+  // "/../public/text.html"));     });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    console.log(__dirname);
+  });
+  // app.use(express.static(publicPath))
 };
